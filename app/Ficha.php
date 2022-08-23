39fa7ba2ab53aeaf149e7f7432dcd6fc57b62934
patoblando <85848059+patoblando@users.noreply.github.com>
2022-08-23 11:08:27 -0300

    Creadaos los archivos de clase Tablero.php y Ficha.php
    Tablero.php permanece vacio.
    Ficha.php ya esta programado.

---
+++ app/Ficha.php
@@ -0,0 +1,21 @@
+
+<?php
+
+//Asigno el espacio de nombres app para todas las variables, clases y metodos.
+namespace App; 
+
+interface FichaInterface {
+	public function return_color(); // Devuelve el color de la ficha
+}
+
+class Ficha implements FichaInterface {
+	protected $color;\
+	
+	public function __construct(String color_ficha){
+		$this->color = $color_ficha;
+	}
+
+	public function return_color(){
+		return $this->color;
+	}
+}
