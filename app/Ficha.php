7e4ae0a398a301a7dff42be290565f30221527c0
patoblando <85848059+patoblando@users.noreply.github.com>
2022-08-23 11:41:16 -0300

    Arreglados errores de Ficha.php. Escrito el codigo de Tablero.php

--- app/Ficha.php
+++ app/Ficha.php
@@ -1,17 +1,15 @@
-
 <?php
 
-//Asigno el espacio de nombres app para todas las variables, clases y metodos.
-namespace App; 
+namespace App; //Asigno el espacio de nombres app para todas las variables, clases y metodos.
 
 interface FichaInterface {
 	public function return_color(); // Devuelve el color de la ficha
 }
 
 class Ficha implements FichaInterface {
-	protected $color;\
-	
-	public function __construct(String color_ficha){
+	protected $color;
+
+	public function __construct(String $color_ficha){
 		$this->color = $color_ficha;
 	}
 
--- app/Tablero.php
+++ app/Tablero.php
@@ -1,1 +1,35 @@
-Tablero.php
+<?php
+
+namespace App; //Asigno el espacio de nombres app para todas las variables, clases y metodos.
+
+use Exception;
+use ColumnaLlenaException;
+
+interface TableroInterf {
+	protected function vaciar_tablero();
+	public function poner_ficha(int $columna, Ficha $ficha);
+}
+
+class Tablero {
+	protected int $ancho
+	protected int $alto
+	protected array $tablero
+
+	protected function vaciar_tablero(){
+		for($i = 0; $i < $alto; $i++){
+			for($a = 0; $a < $ancho; $a++){
+				$this->tablero[i][a] = new Ficha("Vacio");
+			}
+		}
+	}
+
+	public function poner_ficha(int $columna, Ficha $ficha){
+		for($columna_contador = $this->alto; $columna_contador != 0; $columna_contador--){
+			if($this->tablero[$columna_contador][$columna]->color =="Vacio"){
+				tablero[$columna_contador][$columna] = $ficha->color;
+				return;
+			}
+		}
+		throw new ColumnaLlenaException("La columna esta completa.");
+	}
+}
