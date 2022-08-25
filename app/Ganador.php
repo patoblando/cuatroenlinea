04873f232f34fb5a402fea739c6522a608ab006f
patoblando <85848059+patoblando@users.noreply.github.com>
2022-08-25 02:06:54 -0300

    Actualizado Ganador.php
    Añadido un comentario.

--- app/Ganador.php
+++ app/Ganador.php
@@ -7,15 +7,26 @@ interface GanadorInterface {
 }
 
 class Ganador implements GanadorInterface {
-	protected bool $ganadorBandera = True;
+	protected bool $ganador_bandera = False;
 	protected string $ganador = 'Empate';
 
 	//La idea es hacer una funcion recursiva que chequee 3 veces si una ficha
 	//esta en contacto con otra de su mismo color en una direccion inicial
-	private function chequeo_linea(Tablero $tablero, $direc_x, $direc_y, $x, $y, $iteracion){
-		$ficha = $tablero->tablero[$x][$y];
-		$fichaSig = $tablero->tablero[$direc_x][$direc_y];
 
+	//TODO: Esto no fuciona, tengo que buscar la forma en que la funcion avance en una direccion. 
+	//Quizas una funcion auxiliar que dados dos int y un string devuelva la direccion adecuada.
+	private function chequeo_linea(Tablero $tablero, $x, $y, $iteracion){ 
+		if($iteracion == 0){
+			$this->ganador_bandera = True;
+			$this->ganador = '';
+			return;
+		}
+		$ficha = $tablero->tablero[$x][$y];
+		$ficha_sig = $tablero->tablero[$direc_x][$direc_y];
+
+		if($ficha->color == $ficha_sig->color)
+			chequeo_linea($tablero, $direc_x, $direc_y, $x, $y, $iteracion - 1);
+		else return;
 	}
 
 	public function chequeo_juego_ganado(Tablero $tablero, Int $x, Int $y){
@@ -33,28 +44,28 @@ class Ganador implements GanadorInterface {
 		$fichaO  = $tablero->tablero[$x - 1][$y];
 
 		if($ficha->color == $fichaNO->color)
-			chequeoLinea($tablero->tablero, $x - 1, $y + 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x - 1, $y + 1, 3);
 
 		if($ficha->color == $fichaN->color)
-			chequeoLinea($tablero->tablero, $x, $y + 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x, $y + 1, 3);
 
 		if($ficha->color == $fichaNE->color)
-			chequeoLinea($tablero->tablero, $x + 1, $y + 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x + 1, $y + 1, 3);
 
 		if($ficha->color == $fichaE->color)
-			chequeoLinea($tablero->tablero, $x + 1, $y, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x + 1, $y, 3);
 
 		if($ficha->color == $fichaSE->color)
-			chequeoLinea($tablero->tablero, $x + 1, $y - 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x + 1, $y - 1, 3);
 
 		if($ficha->color == $fichaS->color)
-			chequeoLinea($tablero->tablero, $x, $y - 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x, $y - 1, 3);
 
 		if($ficha->color == $fichaSO->color)
-			chequeoLinea($tablero->tablero, $x - 1, $y - 1, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x - 1, $y - 1, 3);
 
 		if($ficha->color == $fichaO->color)
-			chequeoLinea($tablero->tablero, $x - 1, $y, $x, $y, 3);
+			chequeoLinea($tablero->tablero, $x - 1, $y, 3);
 
 	}
 }
